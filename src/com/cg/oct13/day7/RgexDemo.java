527296b33331c956a10af955219cd47064ad09d6
umangsingh145 <66913563+umangsingh145@users.noreply.github.com>
2021-10-26 09:31:58 +0530

    Update RgexDemo.java

--- src/com/cg/oct13/day7/RgexDemo.java
+++ src/com/cg/oct13/day7/RgexDemo.java
@@ -12,9 +12,9 @@ public class RgexDemo {
 
 		
 		
-		String regex = "[a-zA-Z0-9#@\\h]{5}", input = "a  @"; // abc xyz ijk // ABC 123
+	//	String regex = "[a-zA-Z0-9#@\\h]{5}", input = "a  @"; // abc xyz ijk // ABC 123
 //		String regex = "[\\d]{3}", input = "457"; // [0-9]{3}
-		//String regex = "[\\D]{3}", input = "awE"; // [non digit characters]{3}
+		String regex = "[\\D]{3}", input = "awE"; // [non digit characters]{3}
 //		String regex = "[\\w]{3}", input = "4Ya"; // [a-zA-Z0-9]{3}
 //		String regex = "[\\w]{3}", input = "%Ya"; // abc xyz ijk // ABC 123
 
